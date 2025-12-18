b215662ee27bdfad56a80c461a3e13048892432e
MisaelAlves007 <94721492+MisaelAlves007@users.noreply.github.com>
2025-12-18 01:17:11 -0300

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -70,8 +70,8 @@ export default function HeroHome() {
               data-aos="zoom-y-out"
               data-aos-delay={150}
             >
-              The website builder you're <br className="max-lg:hidden" />
-              looking for
+             Personagens que transformam <br className="max-lg:hidden" />
+              festas em momentos inesquecíveis
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -79,8 +79,8 @@ export default function HeroHome() {
                 data-aos="zoom-y-out"
                 data-aos-delay={300}
               >
-                Simple is a modern website builder powered by AI that changes
-                how companies create user interfaces together.
+                Heróis e personagens vivos que encantam crianças, emocionam adultos
+  e transformam aniversários em experiências mágicas.
               </p>
               <div className="relative before:absolute before:inset-0 before:border-y before:[border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1]">
                 <div
@@ -93,7 +93,7 @@ export default function HeroHome() {
                     href="#0"
                   >
                     <span className="relative inline-flex items-center">
-                      Start Free Trial{" "}
+                      Solicitar Orçamento 
                       <span className="ml-1 tracking-normal text-blue-300 transition-transform group-hover:translate-x-0.5">
                         -&gt;
                       </span>
